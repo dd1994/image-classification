395e2d13d312c5668e6f4d0c81238194ba2b4552
JIANG Di <jiang-di@outlook.com>
2024-11-09 21:14:56 +0800

    TEST

--- train.py
+++ train.py
@@ -195,6 +195,7 @@ def cli_main():
                     "init_args": {
                         "project": "identify",
                         "log_model": True,
+                        "mode": "offline",
                         "save_dir": "wandb_logs"
                     }
                 }
