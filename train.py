36054eab683d6ece1ecee586740c947cc8ab25ac
JIANG Di <jiang-di@outlook.com>
2024-11-11 20:15:23 +0800

    fix

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ def cli_main():
             subclass_mode_model=True,
             subclass_mode_data=True,
             save_config_callback=None,
-            seed_everything_default=7747,
+            seed_everything_default=1,
             trainer_defaults={
                 "logger": {
                     "class_path": "pytorch_lightning.loggers.WandbLogger",
