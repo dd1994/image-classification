a80c1ab37e3c12ecc002b149548ec11af0d49bc5
JIANG Di <jiang-di@outlook.com>
2024-11-16 20:18:06 +0800

    fix

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ def cli_main():
             subclass_mode_model=True,
             subclass_mode_data=True,
             save_config_callback=None,
-            seed_everything_default=99,
+            seed_everything_default=1,
             trainer_defaults={
                 "accumulate_grad_batches": 8,
                 "logger": {
@@ -23,7 +23,6 @@ def cli_main():
                     "init_args": {
                         "project": "identify",
                         "log_model": True,
-                        "mode": "offline",
                         "save_dir": "wandb_logs"
                     }
                 }
