463d6682430922c24dd85c52a8deefb96da4f289
JIANG Di <jiang-di@outlook.com>
2025-02-10 20:47:24 +0800

    fix

--- script/process_img.py
+++ script/process_img.py
@@ -45,5 +45,6 @@ def process_images(root_dir):
 
 
 if __name__ == "__main__":
-    root_dir = r"D:\image-classification\data\collection"
+    root_dir = r"D:\image-classification\data\train-mini"
     process_images(root_dir)
+
