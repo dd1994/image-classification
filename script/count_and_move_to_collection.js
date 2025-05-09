8cbc333642b5cb3d7f317552e6a411273084ce36
JIANG Di <jiang-di@outlook.com>
2025-05-09 22:42:37 +0800

    Update count_and_move_to_collection.js

--- script/count_and_move_to_collection.js
+++ script/count_and_move_to_collection.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 const { imgExt } = require('./const')
-const trainDir = 'D:/image-classification/data/train-insecta';
+const trainDir = 'D:/image-classification/data/train';
 const collectionBase = path.join(path.dirname(trainDir), 'genusCollection'); // 自动生成collection路径\
 const ACTIVE = 'spiderGenus'
 const outputFile = `${ACTIVE}_stat.csv`
-const minCount = 80
+const minCount = 50
 // 支持的图片扩展名集合
 const IMAGE_EXTENSIONS = new Set(imgExt);
 
