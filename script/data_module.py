a78946be57bb8eee18e01217b942819ff9e9f366
JIANG Di <jiang-di@outlook.com>
2026-05-07 07:42:23 +0800

    fix

--- script/data_module.py
+++ script/data_module.py
@@ -10,7 +10,7 @@ from util.transform import ToRGBTransform
 
 
 class INatBaseDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir: str = './data/tiny', valid_dir= './data/tiny', id_map_file_path = '', valid_id_map_file_path = '',batch_size: int = 32, num_workers: int = 3, input_size: int = 448, num_classes = 51, rrc_scale_min: float = 0.3, random_erase_prob: float = 0.25):
+    def __init__(self, data_dir: str = './data/tiny', valid_dir= './data/tiny', id_map_file_path = '', valid_id_map_file_path = '',batch_size: int = 32, num_workers: int = 3, input_size: int = 448, num_classes = 51, rrc_scale_min: float = 0.3, random_erase_prob: float = 0.25, re_scale_min: float = 0.02, re_scale_max: float = 0.2):
         super().__init__()
         self.data_dir = data_dir
         self.valid_dir = valid_dir
@@ -22,6 +22,8 @@ class INatBaseDataModule(pl.LightningDataModule):
         self.num_classes = num_classes
         self.rrc_scale_min = rrc_scale_min
         self.random_erase_prob = random_erase_prob
+        self.re_scale_min = re_scale_min
+        self.re_scale_max = re_scale_max
         self.train_dataset = None
         self.test_dataset = None
         self.val_dataset = None
@@ -35,7 +37,7 @@ class INatBaseDataModule(pl.LightningDataModule):
                 v2.RandomHorizontalFlip(p=0.5),
                 v2.ToDtype(torch.float32, scale=True),
                 v2.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-                v2.RandomErasing(p=self.random_erase_prob, scale=(0.02, 0.2), value='random')
+                v2.RandomErasing(p=self.random_erase_prob, scale=(self.re_scale_min, self.re_scale_max), value='random')
             ]),
             'val_test': v2.Compose([
                 ToRGBTransform(),
