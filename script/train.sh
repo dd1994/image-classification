bfe7ddabd13a628c41b68a51556df22dcc0a7695
JIANG Di <jiang-di@outlook.com>
2026-05-16 22:03:40 +0800

    update config

--- script/train.sh
+++ script/train.sh
@@ -1,1 +1,1 @@
-PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True /c/ProgramData/anaconda3/envs/myenv/python.exe ./script/train.py fit --config ./config/mini/swinv2_mini512.json --ckpt_path ./wandb_logs/identify/93fug52i/checkpoints/last.ckpt
+PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True /c/ProgramData/anaconda3/envs/myenv/python.exe ./script/train.py fit --config ./config/tiny/swinv2_tiny.json # --ckpt_path ./wandb_logs/identify/93fug52i/checkpoints/last.ckpt
