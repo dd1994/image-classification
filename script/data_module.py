a19694824851984dbb7c76fc024be945560907a8
JIANG Di <jiang-di@outlook.com>
2026-05-17 23:58:37 +0800

    fix

--- script/data_module.py
+++ script/data_module.py
@@ -61,7 +61,7 @@ class ClassBalancedSampler(Sampler):
 
 
 class INatBaseDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir: str = './data/tiny', valid_dir= './data/tiny', id_map_file_path = '', valid_id_map_file_path = '',batch_size: int = 32, num_workers: int = 3, input_size: int = 448, num_classes = 51, rrc_scale_min: float = 0.3, random_erase_prob: float = 0.25, re_scale_min: float = 0.02, re_scale_max: float = 0.2, rebalance_last_epochs: int = 0, rebalance_floor_count: int = 100):
+    def __init__(self, data_dir: str = './data/tiny', valid_dir= './data/tiny', id_map_file_path = '', valid_id_map_file_path = '',batch_size: int = 32, num_workers: int = 3, input_size: int = 448, num_classes = 51, rrc_scale_min: float = 0.3, random_erase_prob: float = 0.25, re_scale_min: float = 0.02, re_scale_max: float = 0.2, rebalance_last_epochs: int = 0, rebalance_floor_count: int = 100, trivial_augment_prob: float = 1.0):
         super().__init__()
         self.data_dir = data_dir
         self.valid_dir = valid_dir
@@ -77,6 +77,7 @@ class INatBaseDataModule(pl.LightningDataModule):
         self.re_scale_max = re_scale_max
         self.rebalance_last_epochs = rebalance_last_epochs
         self.rebalance_floor_count = rebalance_floor_count
+        self.trivial_augment_prob = trivial_augment_prob
         self.train_dataset = None
         self.test_dataset = None
         self.val_dataset = None
@@ -87,7 +88,7 @@ class INatBaseDataModule(pl.LightningDataModule):
                 v2.ToImage(), # Convert to tensor, only needed if you had a PIL image
                 v2.RandomResizedCrop(self.input_size, scale=(self.rrc_scale_min, 1.0)),
                 v2.RandomApply([v2.RandomRotation(degrees=45, fill="random")], p=0.2),
-                TrivialAugmentWide(),
+                v2.RandomApply([TrivialAugmentWide()], p=self.trivial_augment_prob),
                 v2.RandomHorizontalFlip(p=0.5),
                 v2.ToDtype(torch.float32, scale=True),
                 v2.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
