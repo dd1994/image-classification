599531fc27196c0ab4a4c688ea00319b7414e68c
JIANG Di <jiang-di@outlook.com>
2026-06-28 01:13:33 +0800

    Update data_module.py

--- script/data_module.py
+++ script/data_module.py
@@ -118,8 +118,8 @@ class INatBaseDataModule(pl.LightningDataModule):
                           shuffle=True, num_workers=self.num_workers, persistent_workers=False, prefetch_factor=1)
 
     def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=self.batch_size,
-                          shuffle=False, num_workers=self.num_workers, persistent_workers=False)
+        return DataLoader(self.val_dataset, batch_size=self.batch_size*2,
+                          shuffle=False, num_workers=self.num_workers, persistent_workers=False, prefetch_factor=2)
 
     def test_dataloader(self):
         return DataLoader(self.test_dataset, batch_size=self.batch_size,
