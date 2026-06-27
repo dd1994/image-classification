c3d0177cf424739c680cc9982ca20c8b9d57c7af
JIANG Di <jiang-di@outlook.com>
2026-06-28 01:19:27 +0800

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -11,7 +11,7 @@ for seed in "${SEEDS[@]}"; do
         ./script/train.py fit \
         --config "$CONFIG" \
         --seed_everything="$seed" \
-        --ckpt_path "./wandb_logs/identify/x84v3ram/checkpoints/swinv2-all-step=step=14276.ckpt"
+        --ckpt_path "./wandb_logs/identify/x84v3ram/checkpoints/swinv2-all-epoch=00-step=14276.ckpt"
     echo "Finished seed=$seed"
     echo "Waiting 10s for GPU cleanup..."
     sleep 10
