b666a0f83abd71e63af9582e64d56bd4df4c29dc
JIANG Di <jiang-di@outlook.com>
2026-07-04 10:13:08 +0800

    fix

--- script/train.sh
+++ script/train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CONFIG="./config/fgvc-aves-tiny/eva02_tiny_part2.json"
-SEEDS=(1 42 99)
+CONFIG="./config/fgvc-aves-tiny/eva02_tiny.json"
+SEEDS=(1 42)
 
 for seed in "${SEEDS[@]}"; do
     echo "=========================================="
@@ -10,8 +10,8 @@ for seed in "${SEEDS[@]}"; do
     /c/ProgramData/anaconda3/envs/myenv/python.exe -X faulthandler \
         ./script/train.py fit \
         --config "$CONFIG" \
-        --seed_everything="$seed" \
-        --ckpt_path "./wandb_logs/identify/qwk6q4df/checkpoints/last.ckpt"
+        --seed_everything="$seed"
+        # --ckpt_path "./wandb_logs/identify/qwk6q4df/checkpoints/last.ckpt"
     echo "Finished seed=$seed"
     echo "Waiting 10s for GPU cleanup..."
     sleep 10
